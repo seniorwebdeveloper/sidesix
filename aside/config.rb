22af8d9a12f98556ff90774a312378f155e77fc7
Dan Avery <danavery@sidesix.org>
2012-01-11 00:01:37 -0800

    added aside

---
+++ aside/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/aside"
+http_images_path = "/aside/images"
+http_fonts_path = "/aside/fonts"
+css_dir = "public/aside/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
